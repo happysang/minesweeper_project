c6070b43f50c211fa64e4008f5f0594a283039e1
khs <69028828+happysang@users.noreply.github.com>
2021-11-02 20:24:43 +0900

    기본 틀 생성

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.minesweeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TableLayout table;
+        table = (TableLayout) findViewById(R.id.tableLayout);
+        List<TableRow> tableRows = new ArrayList<>(9);
+        for (int i = 0; i < 9; i++){
+            tableRows.add(new TableRow(this));
+            table.addView(tableRows.get(i));
+        }
+
+        TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(
+                TableRow.LayoutParams.WRAP_CONTENT,
+                TableRow.LayoutParams.WRAP_CONTENT,
+                1.0f);
+
+        Button[][] buttons = new Button[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                buttons[i][j] = new Button(this);
+                buttons[i][j].setLayoutParams(layoutParams);
+                tableRows.get(i).addView(buttons[i][j]);
+            }
+        }
     }
 }
