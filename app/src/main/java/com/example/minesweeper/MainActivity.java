cc81fdb1246aa9592f7561fe05f21128fdc3dbf1
khs <69028828+happysang@users.noreply.github.com>
2021-11-09 15:19:57 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -4,10 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,22 +35,8 @@ public class MainActivity extends AppCompatActivity {
                 1.0f);
 
 
-        // 랜덤으로 폭탄 10개 생성
-        for (int i = 0; i < 10; i++) {
-            Random r = new Random();
-            ArrayList<int[]> bombs = new ArrayList<>();
-            int[] pair = new int[2];
-            pair[0] = r.nextInt(9);
-            pair[1] = r.nextInt(9);
-            if (bombs.contains(pair)) {
-                i--;
-                continue;
-            }
-            bombs.add(pair);
-        }
-
         //버튼 81개 만들고 행 안에 넣음
-        Button[][] buttons = new Button[9][9];
+        BlockButton[][] buttons = new BlockButton[9][9];
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 buttons[i][j] = new BlockButton (this,i,j);
@@ -66,17 +50,27 @@ public class MainActivity extends AppCompatActivity {
 //                        ((BlockButton)view).toggleFlag();}});
             }
         }
-        
 
 
-            BlockButton b = (BlockButton)buttons[pair[0]][pair[1]];
-            b.setMine(true);
+        // 랜덤으로 폭탄 10개 생성
+        for (int i = 0; i < 10; i++) {
+            Random r = new Random();
+            ArrayList<int[]> bombs = new ArrayList<>();
+            int[] pair = new int[2];
+            pair[0] = r.nextInt(9);
+            pair[1] = r.nextInt(9);
+            if (bombs.contains(pair)) {
+                i--;
+                continue;
+            }
+            bombs.add(pair);
+            buttons[pair[0]][pair[1]].setMine(true);
 
             // 클릭 했을 때 breakBlock이 되는지 확인
-//            b.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view){
-//                    ((BlockButton)view).breakBlock();}});
+            buttons[pair[0]][pair[1]].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view){
+                    ((BlockButton)view).breakBlock();}});
         }
     }
 }
